c5b87b95d0d4751b604a569c06de5235e65d4a62
Ladus <roy_biesheuvel@hotmail.com>
2018-04-12 15:17:49 +0200

    Reorganized variables; Added current layer variable; Moved all logic to seperate functions; implemented row and column loop.

--- ProMicro/Example_2x2Key_2Layer/Example_2x2Key_2Layer.ino
+++ ProMicro/Example_2x2Key_2Layer/Example_2x2Key_2Layer.ino
@@ -1,8 +1,18 @@
 #include <Keyboard.h>
+////Config variables
 //Matrix Setup
 const int LAYERS = 2;
 const int ROWS = 2;
 const int COLUMNS = 2;
+
+//Loop Delay
+int delayLoopPin = 21;
+
+//Input pins
+int ColPins[COLUMNS] = {2,3};
+int RowPins[ROWS] = {4,5};
+
+////System Variables
 
 //Default Buttons
 const int NO_ACTION = 0;
@@ -11,37 +21,46 @@ const int LAYER_RAISE = 1;
 //Keymap
 extern int Keymap[LAYERS][ROWS][COLUMNS];
 int PressedButtons[LAYERS][ROWS][COLUMNS];
+int currentLayer = 1; //1 default; 2 raised; 
 
-
-////ADJUST TO YOUR PINS
-//Loop Delay
-int delayLoopPin = 21;
-
-//Input pins
-int ColPins[COLUMNS] = {2,3};
-int RowPins[ROWS] = {4,5};
 
 void setup()
 {
-  pinMode(delayLoopPin, INPUT_PULLUP);
-
-  //Cols
-  for(i=0; i<COLUMNS; i++){
-    pinMode(ColPins[i], OUTPUT);
-  }
-
-  //Rows
-  for(i=0; i<ROWS; i++){
-    pinMode(RowPins[i], INPUT);
-  }
+  setupPins();
 }
 
 void loop()
 {
   delayByPin();
+  switchMatrixLoop();
+}
 
+void setupPins() {
+  pinMode(delayLoopPin, INPUT_PULLUP);
+
+  //Cols
+  for(int i=0; i<COLUMNS; i++){
+    pinMode(ColPins[i], OUTPUT);
+  }
+
+  //Rows
+  for(int i=0; i<ROWS; i++){
+    pinMode(RowPins[i], INPUT);
+  }
+}
+
+void switchMatrixLoop() {
   //loop through columns, loop through rows and read outputs
+  for(int row = 0; row < ROWS; row++) 
+  {
+    for(int column = 0; column < COLUMNS; column++){
+      perSwitchHandler(row, column);
+    }
+  }
   
+}
+
+void perSwitchHandler(int row, int column) {
   //per key check if previously pressed, and not anymore (release key[layer,row,col])
   //else now pressed
   //  check if no action then do nothing
@@ -50,7 +69,7 @@ void loop()
   //  get button from keymap according to layer,row,column number and press
 }
 
-function delayByPin() {
+void delayByPin() {
   //If delayPin becomes low (connected to ground) then delay loop
   //implemented to stop the chip from becoming stuck in a key spamming loop
   while (digitalRead(delayLoopPin) == LOW) 
