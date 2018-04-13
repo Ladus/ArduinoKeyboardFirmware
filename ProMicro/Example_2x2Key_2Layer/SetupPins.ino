2a67ca1002e8971fd502ef11a6c06bd93268b52f
Ladus <roy_biesheuvel@hotmail.com>
2018-04-13 22:52:40 +0200

    Moved key handlers to seperate file, changed matrix loop to pull from rows to allow for the more default diode direction of col > row.

--- ProMicro/Example_2x2Key_2Layer/SetupPins.ino
+++ ProMicro/Example_2x2Key_2Layer/SetupPins.ino
@@ -1,13 +1,13 @@
 void setupPins() {
   pinMode(delayLoopPin, INPUT_PULLUP);
 
-  //Cols
+  //Setup col pins
   for(int i=0; i<COLUMNS; i++){
-    pinMode(ColPins[i], OUTPUT);
+    pinMode(ColPins[i], INPUT_PULLUP);
   }
 
-  //Rows
+  //Setup row pins
   for(int i=0; i<ROWS; i++){
-    pinMode(RowPins[i], INPUT_PULLUP);
+    pinMode(RowPins[i], OUTPUT);
   }
 }
