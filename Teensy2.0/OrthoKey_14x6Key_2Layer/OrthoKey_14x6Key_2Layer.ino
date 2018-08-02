9a6ecaf6cf6a273d5c8e0c684d60affb5b266379
Ladus <roy_biesheuvel@hotmail.com>
2018-08-02 23:08:42 +0200

    Added new teensy based 6x14 keyboard

---
+++ Teensy2.0/OrthoKey_14x6Key_2Layer/OrthoKey_14x6Key_2Layer.ino
@@ -0,0 +1,48 @@
+#include <Keyboard.h>
+////Config variables
+//Matrix Setup
+const int LAYERS = 1;
+const int ROWS = 6;
+const int COLUMNS = 14;
+
+//Loop Delay
+int delayLoopPin = 21;
+
+//Input pins
+int ColPins[COLUMNS] = {0,1,2,3,4,5,6,7,8,9,10,12,13,14};
+int RowPins[ROWS] = {20,19,18,17,16,15};
+
+////System Variables
+
+//Default Buttons
+const int NO_ACTION = 0;
+const int LAYER_RAISE = 1;
+const int LAYER_LOWER = 2;
+
+//Keymap
+bool switchStates[ROWS][COLUMNS];
+int currentLayer = 0 //0 raised; 1 default; 
+
+
+void setup()
+{
+  setupPins();
+  Serial.begin(9600); 
+}
+
+void loop()
+{
+  delayByPin();
+  switchMatrixLoop();
+  delay(10);
+}
+
+void delayByPin() {
+  //If delayPin becomes low (connected to ground) then delay loop
+  //implemented to stop the chip from becoming stuck in a key spamming loop
+  //Note: Can be removed if in need of the extra pin
+  while (digitalRead(delayLoopPin) == LOW) 
+  {
+    delay(8000);
+  }
+}
