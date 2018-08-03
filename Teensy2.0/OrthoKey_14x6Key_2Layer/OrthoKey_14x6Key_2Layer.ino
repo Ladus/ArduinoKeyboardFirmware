820a364a73b6dba9fe6c44a75b5eaa966febfc94
Ladus <roy_biesheuvel@hotmail.com>
2018-08-03 11:31:21 +0200

    Updated key layout

--- Teensy2.0/OrthoKey_14x6Key_2Layer/OrthoKey_14x6Key_2Layer.ino
+++ Teensy2.0/OrthoKey_14x6Key_2Layer/OrthoKey_14x6Key_2Layer.ino
@@ -10,7 +10,7 @@ int delayLoopPin = 21;
 
 //Input pins
 int ColPins[COLUMNS] = {0,1,2,3,4,5,6,7,8,9,10,12,13,14};
-int RowPins[ROWS] = {20,19,18,17,16,15};
+int RowPins[ROWS] = {20,19,22,17,16,15};
 
 ////System Variables
 
