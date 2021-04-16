63702a6387f5b3987c60913fae788834a6b8b2cb
Roy Biesheuvel <roy_biesheuvel@hotmail.com>
2021-04-16 17:16:07 +0200

    enabled debugging, and fixed bugs

--- OrthoKey_12x4Key_3Layer/OrthoKey_12x4Key_3Layer.ino
+++ OrthoKey_12x4Key_3Layer/OrthoKey_12x4Key_3Layer.ino
@@ -6,27 +6,27 @@ const int LAYERS = 1;
 const int ROWS = 4;
 const int COLUMNS = 12;
 
+//Loop Delay
+int delayLoopPin = 21;
+
 //Input pins
-<<<<<<< HEAD:OrthoKey_12x4Key_3Layer/OrthoKey_12x4Key_2Layer.ino
 int ColPins[COLUMNS] = {21,20,19,18,17,11,5,4,3,2,1,0};
 int RowPins[ROWS] = {10,9,13,12};
-=======
-const int ColPins[COLUMNS] = {6,7,8,9,10,16,14,15,18,19,20,21};
-const int RowPins[ROWS] = {2,3,4,5};
->>>>>>> master:ProMicro/Planck_12x4Key_3Layer/Planck_12x4Key_3Layer.ino
 
 //Output debug info to serial monitor
 const bool EnableDebug = true;
 
 ////System Variables
+
 //Default Buttons
-const byte NO_ACTION = 0;
-const byte LAYER_RAISE = 1;
-const byte LAYER_LOWER = 2;
+const int NO_ACTION = 0;
+const int LAYER_RAISE = 1;
+const int LAYER_LOWER = 2;
 
 //Keymap
 bool switchStates[ROWS][COLUMNS];
-int currentLayer = 1; //0 raised; 1 default; 2 lowered;
+int currentLayer = 1; //0 raised; 1 default; 
+
 
 void setup()
 {
@@ -49,5 +49,6 @@ void loop()
 {
   delayByPin();
   switchMatrixLoop();
+  DebugPrintKeyState();
   delay(10);
 }
